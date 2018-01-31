a82162905cec27a83321ec080fbeaa4d258d5198
Edouard Leurent <eleurent@gmail.com>
2018-01-31 12:56:21 +0100

    Add an instance of an insertion problem

---
+++ problems/insertion.py
@@ -0,0 +1,41 @@
+from __future__ import division, print_function
+import numpy as np
+from highway.road import LineType, StraightLane, LanesConcatenation, Road
+from highway.vehicle import Obstacle, ControlledVehicle, IDMVehicle
+from highway.simulation import Simulation
+
+
+def make_road():
+    ends = [100, 20, 100]
+    l0 = StraightLane(np.array([0,0]), 0, 4.0, [LineType.CONTINUOUS, LineType.NONE])
+    l1 = StraightLane(np.array([0,4]), 0, 4.0, [LineType.STRIPED, LineType.CONTINUOUS])
+
+    lc0 = StraightLane(np.array([0,6.5+4+4]), 0, 4.0, [LineType.CONTINUOUS, LineType.CONTINUOUS])
+    lc1 = StraightLane(lc0.position(ends[0],0), -20*3.14159/180, 4.0, [LineType.CONTINUOUS, LineType.CONTINUOUS])
+    lc2 = StraightLane(lc1.position(ends[1],0), 0, 4.0, [LineType.NONE, LineType.CONTINUOUS])
+    l2 = LanesConcatenation([lc0, lc1, lc2], ends)
+    road = Road([l0, l1, l2])
+    road.vehicles.append(Obstacle(road, lc2.position(ends[2], 0)))
+    return road, l1, l2
+
+
+def make_vehicles(road, highway_lane_2, insertion_lane):
+    vehicle = IDMVehicle(road, insertion_lane.position(0, 0))
+    ego_vehicle = ControlledVehicle(road, highway_lane_2.position(0,0))
+    road.vehicles.append(vehicle)
+    road.vehicles.append(ego_vehicle)
+    return ego_vehicle
+
+
+def run():
+    road, highway_lane_2, insertion_lane = make_road()
+    ego_vehicle = make_vehicles(road, highway_lane_2, insertion_lane)
+    sim = Simulation(road)
+    sim.vehicle = ego_vehicle
+
+    while not sim.done:
+        sim.process()
+    sim.quit()
+
+if __name__ == '__main__':
+    run()
