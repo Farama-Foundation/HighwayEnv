de33a04281ac88e7c2d3d3e06d824e216f1227b4
Edouard Leurent <eleurent@gmail.com>
2018-02-28 17:32:06 +0100

    Add some make commands for Sphinx
    - make apidoc
    - make html
    - make http

--- Makefile
+++ Makefile
@@ -14,6 +14,14 @@ help:
 
 .PHONY: help Makefile
 
+apidoc:
+	sphinx-apidoc -o docs -e highway
+
+http:
+	python -mwebbrowser "http://localhost:8000/_build/html/"
+	python -m SimpleHTTPServer
+
+
 # Catch-all target: route all unknown targets to Sphinx using the new
 # "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
 %: Makefile
--- conf.py
+++ conf.py
@@ -44,6 +44,9 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+autodoc_default_flags = ['members', 'private-members', 'undoc-members', 'special-members']
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -77,7 +80,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
