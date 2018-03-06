277228d66268784b6b44165325285f288f520ae8
Edouard Leurent <eleurent@gmail.com>
2018-03-06 16:11:14 +0100

    Update offline linear model regression

--- problems/model_fitting/offline_model_fitting.py
+++ problems/model_fitting/offline_model_fitting.py
@@ -2,21 +2,27 @@ from __future__ import division, print_function
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from highway.road import Road
+from highway.road.road import Road
 from highway.simulation import Simulation
 
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 from highway.vehicle.behavior import IDMVehicle, LinearVehicle
-from highway.vehicle.dynamics import Vehicle, Obstacle
+from highway.vehicle.dynamics import Obstacle
 
 
 def generate_data(count):
-    Vehicle.COLLISIONS_ENABLED = False
-    road = Road.create_random_road(lanes_count=2, lane_width=4.0, vehicles_count=5, vehicles_type=IDMVehicle)
-    sim = Simulation(road, ego_vehicle_type=IDMVehicle, displayed=True)
-    road.vehicles.append(Obstacle(road, road.vehicles[0].position + np.array([30, 0])))
+    # Vehicle.COLLISIONS_ENABLED = False
+    vehicle_type = IDMVehicle
+    road = Road.create_random_road(lanes_count=2, lane_width=4.0, vehicles_count=5, vehicles_type=vehicle_type)
+    sim = Simulation(road, ego_vehicle_type=vehicle_type, displayed=True)
+    sim.RECORD_VIDEO = False
+    road.add_random_vehicles(5, vehicles_type=vehicle_type)
+    road.vehicles.append(Obstacle(road, np.array([50., 0])))
+    road.vehicles.append(Obstacle(road, np.array([130., 4.])))
+    # for v in road.vehicles:
+    #     v.enable_lane_change = False
 
     for _ in range(count):
         sim.handle_events()
@@ -25,7 +31,7 @@ def generate_data(count):
         sim.step()
         sim.display()
     sim.quit()
-    return sim.road.get_log()
+    return [v.get_log() for v in road.vehicles]
 
 
 def get_features(data):
@@ -33,7 +39,7 @@ def get_features(data):
     d_safe = LinearVehicle.DISTANCE_WANTED + data['v'] * LinearVehicle.TIME_WANTED + LinearVehicle.LENGTH
     return pd.concat([v0 - data['v'],
                       np.minimum(data['front_v'] - data['v'], 0),
-                      np.maximum(data['rear_v'] - data['v'], 0),
+                      0*np.maximum(data['rear_v'] - data['v'], 0),
                       np.minimum(data['front_distance'] - d_safe, 0),
                       np.maximum(d_safe - data['rear_distance'], 0)
                       ], axis=1)
@@ -41,29 +47,25 @@ def get_features(data):
 
 def fit(dump):
     regr = linear_model.LinearRegression()
-    data = dump[dump['front_distance'].notnull()
-                & dump['rear_distance'].notnull()].reset_index(drop=True)
+    try:
+        data = dump[dump['front_distance'].notnull()
+                    & dump['rear_distance'].notnull()].reset_index(drop=True)
+    except KeyError:
+        return None, None
 
     X = get_features(data)
     y = data['acceleration']
 
-    data_fit = data[(-4.9 < data['acceleration'])
-                    & (data['acceleration'] < 2.9)].reset_index(drop=True)
+    data_fit = data[(-0.95*IDMVehicle.ACC_MAX < data['acceleration'])
+                    & (data['acceleration'] < 0.95*IDMVehicle.ACC_MAX)].reset_index(drop=True)
     X_fit = get_features(data_fit)
     y_fit = data_fit['acceleration']
 
     regr.fit(X_fit, y_fit)
     print(regr.coef_)
-    y_pred = np.clip(regr.predict(X), -IDMVehicle.BRAKE_ACC, IDMVehicle.ACC_MAX)
-    display(y, y_pred)
-
-    regr.coef_[0] = 1
-    regr.coef_[1] = 2
-    regr.coef_[2] = 0
-    regr.coef_[3] = 10
-    regr.coef_[4] = 0
-    y_pred = np.clip(regr.predict(X), -IDMVehicle.BRAKE_ACC, IDMVehicle.ACC_MAX)
-    display(y, y_pred)
+    y_pred = np.clip(regr.predict(X), -IDMVehicle.ACC_MAX, IDMVehicle.ACC_MAX)
+    # display(y, y_pred)
+    return y, y_pred
 
 
 def display(y, y_pred):
@@ -83,7 +85,7 @@ def display(y, y_pred):
     plt.show()
 
     plt.figure()
-    plt.hist(y_pred - y, weights=np.zeros_like(y) + 100. / y.size)
+    plt.hist(y_pred - y, bins=30, weights=np.zeros_like(y) + 100. / y.size)
     plt.xlabel(r'Acceleration error [$m/s^2$]')
     plt.ylabel(r'Frequency [%]')
     plt.show()
@@ -91,10 +93,16 @@ def display(y, y_pred):
 
 def main():
     print('Generating data...')
-    dump = generate_data(30 * 10)
+    dumps = generate_data(30 * 30)
     print('Generation done.')
-    # display(dump)
-    fit(dump)
+    yy = np.array([])
+    yyp = np.array([])
+    for d in dumps:
+        y, y_pred = fit(d)
+        if y is not None:
+            yy = np.concatenate((yy, y))
+            yyp = np.concatenate((yyp, y_pred))
+    display(yy, yyp)
 
 
 if __name__ == '__main__':
