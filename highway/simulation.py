abb7293b8c2bcae38d16bcedccbbdcfeba37a25e
Edouard Leurent <eleurent@gmail.com>
2018-03-14 10:42:44 +0100

    Fix minor out of bounds error

--- highway/simulation.py
+++ highway/simulation.py
@@ -127,8 +127,8 @@ class Simulation:
             self.frame_count += 1
             if self.t > self.MAXIMUM_VIDEO_LENGTH \
                     or (self.vehicle.crashed and self.vehicle.velocity < 1) \
-                    or self.vehicle.position[0] > 50 + np.max([o.position[0] for o in self.road.vehicles
-                                                                if o is not self.vehicle]):
+                    or ((len(self.road.vehicles) > 1) and (self.vehicle.position[0] > 50
+                        + np.max([o.position[0] for o in self.road.vehicles if o is not self.vehicle]))):
                 os.system("ffmpeg -r 60 -i {0}/{2}_%d.bmp -vcodec libx264 -crf 25 {1}/{2}.avi"
                           .format(self.TMP_FOLDER, self.OUT_FOLDER, self.video_name))
                 self.done = True
