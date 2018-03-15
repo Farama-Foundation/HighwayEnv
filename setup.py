f138a20ffd420a686c5137ba4926d3e92b2c4870
Edouard Leurent <eleurent@gmail.com>
2018-03-15 13:11:18 +0100

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='highway-env',
+    version='1.0.dev0',
+    description='An environment for simulated highway driving tasks',
+    url='https://github.com/eleurent/highway-env',
+    author='Edouard Leurent',
+    author_email='eleurent@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Researchers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='autonomous highway driving simulation environment reinforcement learning',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['numpy', 'pygame', 'jupyter'],
+    extras_require={
+        'dev': ['scipy', 'matplotlib', 'pandas'],
+    },
+    entry_points={
+        'console_scripts': [],
+    },
+)
+
