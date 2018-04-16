e6cc55b1fd52970bd44d60990084907090a1cf23
Edouard Leurent <eleurent@gmail.com>
2018-04-16 17:37:45 +0200

    Analyse latest run by default

--- highway_env/wrappers/monitor.py
+++ highway_env/wrappers/monitor.py
@@ -8,12 +8,13 @@ from gym.wrappers.monitoring import video_recorder
 
 
 class MonitorV2(Monitor):
+    RUN_PREFIX = 'run'
 
     def __init__(self, env, directory, video_callable=None, force=False, resume=False,
                  write_upon_reset=False, uid=None, mode=None):
         if video_callable is None:
             video_callable = MonitorV2.always_call_video
-        directory = self.postfix_directory(directory)
+        directory = self.run_directory(directory)
         super(MonitorV2, self).__init__(env, directory, video_callable, force, resume, write_upon_reset, uid, mode)
 
     def close(self):
@@ -40,6 +41,6 @@ class MonitorV2(Monitor):
     def always_call_video(i):
         return True
 
-    @staticmethod
-    def postfix_directory(directory):
-        return os.path.join(directory, 'run_{}'.format(datetime.datetime.now().strftime('%Y%m%d-%H%M%S')))
+    def run_directory(self, directory):
+        return os.path.join(directory, '{}_{}'.format(self.RUN_PREFIX,
+                                                      datetime.datetime.now().strftime('%Y%m%d-%H%M%S')))
--- tests/analyse_tests.py
+++ tests/analyse_tests.py
@@ -1,18 +1,31 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from gym.wrappers.monitor import load_results
 
+from highway_env.wrappers.monitor import MonitorV2
+
+OUT_DIRECTORY = 'out'
+
+
+def find_latest_run():
+    files = sorted(os.listdir(OUT_DIRECTORY))
+    return [os.path.join(OUT_DIRECTORY, f) for f in files if f.startswith(MonitorV2.RUN_PREFIX + '_')][-1]
+
 
 def analyse(run_directory):
+    print('Analysing directory', run_directory)
     manifest = load_results(run_directory)
+    plot(manifest['episode_rewards'], 'rewards')
     histogram(manifest['episode_rewards'], 'rewards')
     histogram(manifest['episode_lengths'], 'episode lengths')
-    plot(manifest['episode_rewards'], 'rewards')
-    scatter(manifest['episode_lengths'], manifest['episode_rewards'], 'lengths', 'rewards')
+    # scatter(manifest['episode_lengths'], manifest['episode_rewards'], 'lengths', 'rewards')
 
 
 def histogram(data, title):
+    plt.figure()
     plt.grid(True)
     plt.hist(data,
              weights=np.zeros_like(data) + 1. / len(data), bins=20)
@@ -23,15 +36,17 @@ def histogram(data, title):
 
 
 def plot(data, title):
+    plt.figure()
     plt.grid(True)
     plt.plot(np.arange(np.size(data)), data)
     plt.title('History of {}'.format(title))
-    plt.xlabel(title.capitalize())
-    plt.ylabel('Frequency')
+    plt.xlabel('Runs')
+    plt.ylabel(title.capitalize())
     plt.show()
 
 
 def scatter(xx, yy, title_x, title_y):
+    plt.figure()
     plt.grid(True)
     plt.scatter(xx, yy)
     plt.title('{} with respect to {}'.format(title_x, title_y))
@@ -41,4 +56,6 @@ def scatter(xx, yy, title_x, title_y):
 
 
 if __name__ == '__main__':
-    analyse('out/run_20180413-171104')
+    # analyse('{}/{}'.format(OUT_DIRECTORY, 'run_20180416-170328'))
+    analyse(find_latest_run())
+
