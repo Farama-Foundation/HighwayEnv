fdb42187b291f3b1aa6d0156e3eb782d7d83a449
Edouard Leurent <eleurent@gmail.com>
2018-05-25 18:10:25 +0200

    Update RunAnalyzer api

--- scripts/analyse_runs.py
+++ scripts/analyse_runs.py
@@ -1,6 +1,11 @@
 from rl_agents.trainer.analyzer import RunAnalyzer
 
 if __name__ == '__main__':
-    RunAnalyzer('out', ['100_mcts_subtree', '100_mcts_prior', '100_ttcvi'])
+    RunAnalyzer(['out/HighwayEnv/DQNPytorchAgent/e50000',
+                 'out/HighwayEnv/MCTSAgent/avg_i25_e80',
+                 'out/HighwayEnv/MCTSAgent/avg_i75_e80',
+                 'out/HighwayEnv/MCTSWithPriorPolicyAgent/eps09_i25_t10',
+                 'out/HighwayEnv/MCTSWithPriorPolicyAgent/eps08_i25_t10',
+                 'out/HighwayEnv/MCTSWithPriorPolicyAgent/eps03_i25_t10'])
 
 
