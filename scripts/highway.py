a474afde0a0bbb1fa5b85026c9d266d4fbbb952a
Edouard Leurent <eleurent@gmail.com>
2018-05-26 18:14:30 +0200

    Match rl_agents exploration policy api

--- scripts/highway.py
+++ scripts/highway.py
@@ -4,7 +4,10 @@ import gym
 
 from highway_env.vehicle.behavior import IDMVehicle
 from rl_agents.agents.dqn.dqn_pytorch import DQNPytorchAgent
+from rl_agents.agents.exploration.boltzmann import Boltzmann
+from rl_agents.agents.exploration.epsilon_greedy import EpsilonGreedy
 from rl_agents.agents.tree_search.mcts import MCTSAgent, MCTSWithPriorPolicyAgent
+from rl_agents.configuration import Config
 from rl_agents.trainer.benchmark import Benchmark
 from rl_agents.trainer.simulation import Simulation
 
@@ -18,7 +21,7 @@ def main():
 def prepare_environment():
     gym.logger.set_level(gym.logger.INFO)
     env = gym.make('highway-v0')
-    env.set_difficulty_level("HARD")
+    env.set_difficulty_level("EASY")
     return env
 
 
@@ -33,26 +36,25 @@ def evaluate_agent(env):
 
 def benchmark_agents(env):
     # agents = [mcts(env, iterations=20), mcts(env, iterations=60), mcts(env, iterations=100)]
-    agents = [mcts_with_prior(env, epsilon=0.3),
-              mcts_with_prior(env, epsilon=0.5),
-              mcts_with_prior(env, epsilon=0.7),
-              mcts_with_prior(env, epsilon=0.8),
-              mcts_with_prior(env, epsilon=0.9),
-              mcts_with_prior(env, epsilon=1.0)]
+    agents = [mcts_with_prior(env, temperature=0.0),
+              mcts_with_prior(env, temperature=0.1),
+              mcts_with_prior(env, temperature=0.3),
+              mcts_with_prior(env, temperature=0.5),
+              mcts_with_prior(env, temperature=0.7),
+              mcts_with_prior(env, temperature=1.0)]
     benchmark = Benchmark(env, agents, num_episodes=25)
     benchmark.run()
 
 
 def dqn_pytorch(environment):
-    config = {
-        "layers": [256, 256],
-        "memory_capacity": 50000,
-        "batch_size": 100,
-        "gamma": 0.9,
-        "epsilon": [1.0, 0.01],
-        "epsilon_tau": 50000*2,
-        "target_update": 1
-    }
+    config = Config(layers=[256, 256],
+                    memory_capacity=50000,
+                    batch_size=100,
+                    gamma=0.9,
+                    exploration=Config(method=EpsilonGreedy,
+                                       epsilon=[1.0, 0.1],
+                                       epsilon_tau=50000),
+                    target_update=1)
     return DQNPytorchAgent(environment, config)
 
 
@@ -67,16 +69,14 @@ def mcts(environment, iterations=75, temperature=10):
                      max_depth=7)
 
 
-def mcts_with_prior(environment, epsilon=0.5):
-    config = {
-        "layers": [256, 256],
-        "memory_capacity": 50000,
-        "batch_size": 100,
-        "gamma": 0.9,
-        "epsilon": [epsilon, epsilon],
-        "epsilon_tau": 50000*2,
-        "target_update": 1
-    }
+def mcts_with_prior(environment, temperature=0.5):
+    config = Config(layers=[256, 256],
+                    memory_capacity=50000,
+                    batch_size=100,
+                    gamma=0.9,
+                    exploration=Config(method=Boltzmann,
+                                       temperature=temperature),
+                    target_update=1)
     prior_agent = DQNPytorchAgent(environment, config)
     prior_agent.load('out/HighwayEnv/DQNPytorchAgent/saved_models/easy_15000.tar')
     return MCTSWithPriorPolicyAgent(environment,
