b33fbb7648dd43f446d08a68543e805a4317d15d
Edouard Leurent <eleurent@gmail.com>
2018-09-11 17:19:59 +0200

    Enhance roundabout network

--- highway_env/envs/roundabout_env.py
+++ highway_env/envs/roundabout_env.py
@@ -4,7 +4,7 @@ import numpy as np
 from highway_env import utils
 from highway_env.envs.abstract import AbstractEnv
 from highway_env.envs.graphics import EnvViewer
-from highway_env.road.lane import LineType, StraightLane, CircularLane
+from highway_env.road.lane import LineType, StraightLane, CircularLane, SineLane
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.control import MDPVehicle
 
@@ -48,23 +48,42 @@ class RoundaboutEnv(AbstractEnv):
         return self._observation()
 
     def make_road(self):
-        length = 40
-        center = [0, -length]
-        radius = [length, length+4]
-        line = [[LineType.CONTINUOUS, LineType.STRIPED], [LineType.NONE, LineType.CONTINUOUS]]
-        alpha = 10
+        # Circle lanes: (s)outh/(e)ast/(n)orth/(w)est (e)ntry/e(x)it.
+        center = [0, 0]  # [m]
+        radius = 40  # [m]
+        alpha = 30  # [deg]
 
         net = RoadNetwork()
+        radii = [radius, radius+4]
+        n, c, s = LineType.NONE, LineType.CONTINUOUS, LineType.STRIPED
+        line = [[c, s], [n, c]]
         for lane in [0, 1]:
-            net.add_lane(0, 1, CircularLane(center, radius[lane], rad(90-alpha), rad(alpha), line_types=line[lane]))
-            net.add_lane(1, 2, CircularLane(center, radius[lane], rad(alpha), rad(-alpha), line_types=line[lane]))
-            net.add_lane(2, 3, CircularLane(center, radius[lane], rad(-alpha), rad(-90+alpha), line_types=line[lane]))
-            net.add_lane(3, 4, CircularLane(center, radius[lane], rad(-90+alpha), rad(-90-alpha), line_types=line[lane]))
-            net.add_lane(4, 5, CircularLane(center, radius[lane], rad(-90-alpha), rad(-180+alpha), line_types=line[lane]))
-            net.add_lane(5, 6, CircularLane(center, radius[lane], rad(-180+alpha), rad(-180-alpha), line_types=line[lane]))
-            net.add_lane(6, 7, CircularLane(center, radius[lane], rad(180-alpha), rad(90+alpha), line_types=line[lane]))
-            net.add_lane(7, 0, CircularLane(center, radius[lane], rad(90+alpha), rad(90-alpha), line_types=line[lane]))
-        net.add_lane(10, 0, StraightLane([0, 50], [10, 6]))
+            net.add_lane("se", "ex", CircularLane(center, radii[lane], rad(90-alpha), rad(alpha), line_types=line[lane]))
+            net.add_lane("ex", "ee", CircularLane(center, radii[lane], rad(alpha), rad(-alpha), line_types=line[lane]))
+            net.add_lane("ee", "nx", CircularLane(center, radii[lane], rad(-alpha), rad(-90+alpha), line_types=line[lane]))
+            net.add_lane("nx", "ne", CircularLane(center, radii[lane], rad(-90+alpha), rad(-90-alpha), line_types=line[lane]))
+            net.add_lane("ne", "wx", CircularLane(center, radii[lane], rad(-90-alpha), rad(-180+alpha), line_types=line[lane]))
+            net.add_lane("wx", "we", CircularLane(center, radii[lane], rad(-180+alpha), rad(-180-alpha), line_types=line[lane]))
+            net.add_lane("we", "sx", CircularLane(center, radii[lane], rad(180-alpha), rad(90+alpha), line_types=line[lane]))
+            net.add_lane("sx", "se", CircularLane(center, radii[lane], rad(90+alpha), rad(90-alpha), line_types=line[lane]))
+
+        # Access lanes: (r)oad/(s)ine
+        access = 200  # [m]
+        dev = 160  # [m]
+        a = 5  # [m]
+        delta_st = 0.21*dev  # [m]
+
+        delta_en = dev-delta_st
+        w = 2*np.pi/dev
+        net.add_lane("ser", "ses", StraightLane([2, access], [2, dev/2], line_types=[s, c]))
+        net.add_lane("ses", "se", SineLane([2+a, dev/2], [2+a, dev/2-delta_st], a, w, -np.pi/2, line_types=[c, c]))
+        net.add_lane("sx", "sxs", SineLane([-2-a, -dev/2+delta_en], [-2-a, dev/2], a, w, -np.pi/2+w*delta_en, line_types=[c, c]))
+        net.add_lane("sxs", "sxr", StraightLane([-2, dev / 2], [-2, access], line_types=[n, c]))
+
+        net.add_lane("eer", "ees", StraightLane([access, -2], [dev / 2, -2], line_types=[s, c]))
+        net.add_lane("ees", "ee", SineLane([dev / 2, -2-a], [dev / 2 - delta_st, -2-a], a, w, -np.pi / 2, line_types=[c, c]))
+        net.add_lane("ex", "exs", SineLane([-dev / 2 + delta_en, 2+a], [dev / 2, 2+a], a, w, -np.pi / 2 + w * delta_en, line_types=[c, c]))
+        net.add_lane("exs", "exr", StraightLane([dev / 2, 2], [access, 2], line_types=[n, c]))
 
         road = Road(network=net)
         self.road = road
@@ -75,10 +94,11 @@ class RoundaboutEnv(AbstractEnv):
         :return: the ego-vehicle
         """
         road = self.road
+        ego_lane = road.network.get_lane(("ser", "ses", 0))
         ego_vehicle = MDPVehicle(road,
-                                 road.network.get_lane((10, 0, 0)).position(0, 0),
+                                 ego_lane.position(90, 0),
                                  velocity=10,
-                                 heading=road.network.get_lane((10, 0, 0)).heading)
+                                 heading=ego_lane.heading_at(90))
         MDPVehicle.SPEED_MIN = 5
         MDPVehicle.SPEED_MAX = 20
         road.vehicles.append(ego_vehicle)
@@ -87,9 +107,9 @@ class RoundaboutEnv(AbstractEnv):
         other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
         for i in range(3):
             road.vehicles.append(other_vehicles_type(road,
-                                                     road.network.get_lane((6, 7, 0)).position(20*i, 0),
+                                                     road.network.get_lane(("we", "sx", 0)).position(20*i, 0),
                                                      velocity=10,
-                                                     heading=road.network.get_lane((6, 7, 0)).heading_at(20*i)))
+                                                     heading=road.network.get_lane(("we", "sx", 0)).heading_at(20*i)))
 
 
 def rad(deg):
