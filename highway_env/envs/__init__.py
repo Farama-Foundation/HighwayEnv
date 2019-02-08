b63d5cb30cb1522e8e0486c741e746896e351233
Edouard Leurent <eleurent@gmail.com>
2019-02-08 21:33:22 +0100

    Remove all non-media files
    Media branch only for storage of heavy files

--- highway_env/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from highway_env.envs.highway_env import HighwayEnv
-from highway_env.envs.merge_env import MergeEnv
-from highway_env.envs.roundabout_env import RoundaboutEnv
-from highway_env.envs.parking_env import ParkingEnv
