e13a76a9a9860bfdfc4b4dc3afdc85faa5933111
Edouard Leurent <eleurent@gmail.com>
2019-10-20 17:05:05 +0200

    Do not show history by default

--- highway_env/envs/two_way_env.py
+++ highway_env/envs/two_way_env.py
@@ -31,7 +31,8 @@ class TwoWayEnv(AbstractEnv):
         "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
         "screen_width": 600,
         "screen_height": 150,
-        "centering_position": [0.3, 0.5]
+        "centering_position": [0.3, 0.5],
+        "show_history": False
     }
 
     def __init__(self):
