c33f76b950fbc6fa7e5164c38696ab1bc0d865ea
Edouard Leurent <eleurent@gmail.com>
2020-01-22 16:10:41 +0100

    Proper rotation of vehicles graphics
    Finally!

--- highway_env/vehicle/graphics.py
+++ highway_env/vehicle/graphics.py
@@ -40,8 +40,36 @@ class VehicleGraphics(object):
         if not offscreen:  # convert_alpha throws errors in offscreen mode TODO() Explain why
             s = pygame.Surface.convert_alpha(s)
         h = v.heading if abs(v.heading) > 2 * np.pi / 180 else 0
-        sr = pygame.transform.rotate(s, -h * 180 / np.pi)
-        surface.blit(sr, (surface.pos2pix(v.position[0] - v.LENGTH / 2, v.position[1] - v.LENGTH / 2)))
+        # Centered rotation
+        position = (surface.pos2pix(v.position[0], v.position[1]))
+        cls.blitRotate(surface, s, position, np.rad2deg(-h))
+
+    @staticmethod
+    def blitRotate(surf, image, pos, angle, origin_pos=None, show_rect=False):
+        """Many thanks to https://stackoverflow.com/a/54714144 """
+        # calculate the axis aligned bounding box of the rotated image
+        w, h = image.get_size()
+        box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
+        box_rotate = [p.rotate(angle) for p in box]
+        min_box = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1])
+        max_box = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1])
+
+        # calculate the translation of the pivot
+        if origin_pos is None:
+            origin_pos = w / 2, h / 2
+        pivot = pygame.math.Vector2(origin_pos[0], -origin_pos[1])
+        pivot_rotate = pivot.rotate(angle)
+        pivot_move = pivot_rotate - pivot
+
+        # calculate the upper left origin of the rotated image
+        origin = (pos[0] - origin_pos[0] + min_box[0] - pivot_move[0], pos[1] - origin_pos[1] - max_box[1] + pivot_move[1])
+        # get a rotated image
+        rotated_image = pygame.transform.rotate(image, angle)
+        # rotate and blit the image
+        surf.blit(rotated_image, origin)
+        # draw rectangle around the image
+        if show_rect:
+            pygame.draw.rect (surf, (255, 0, 0), (*origin, *rotated_image.get_size()), 2)
 
     @classmethod
     def display_trajectory(cls, states, surface, offscreen=False):
