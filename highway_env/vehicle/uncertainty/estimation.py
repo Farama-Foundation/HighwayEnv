51b1ca90f3d6310a833d216f1e2dfc576cbcaec1
Edouard Leurent <eleurent@gmail.com>
2020-02-12 14:18:58 +0100

    Fix model rejection

--- highway_env/vehicle/uncertainty/estimation.py
+++ highway_env/vehicle/uncertainty/estimation.py
@@ -15,34 +15,48 @@ class RegressionVehicle(IntervalVehicle):
     def estimate(data, lambda_=1e-5, sigma=0.05):
         phi = np.array(data["features"])
         y = np.array(data["outputs"])
-        G_N_lambda = 1/sigma * np.transpose(phi) @ phi + lambda_ * np.identity(phi.shape[-1])
-        theta_N_lambda = np.linalg.inv(G_N_lambda) @ np.transpose(phi) @ y / sigma
-        return theta_N_lambda, G_N_lambda
+        g_n_lambda = 1/sigma * np.transpose(phi) @ phi + lambda_ * np.identity(phi.shape[-1])
+        theta_n_lambda = np.linalg.inv(g_n_lambda) @ np.transpose(phi) @ y / sigma
+        return theta_n_lambda, g_n_lambda
 
     @staticmethod
-    def parameter_polytope(data, delta, param_bound, lambda_=1e-5):
-        theta_N_lambda, G_N_lambda = RegressionVehicle.estimate(data)
-        d = G_N_lambda.shape[0]
-        beta_n = np.sqrt(2*np.log(np.sqrt(np.linalg.det(G_N_lambda) / lambda_ ** d) / delta)) + \
+    def parameter_polytope(data, delta, parameter_box, lambda_=1e-5):
+        param_bound = np.amax(np.abs(parameter_box))
+        theta_n_lambda, g_n_lambda = RegressionVehicle.estimate(data)
+        d = g_n_lambda.shape[0]
+        beta_n = np.sqrt(2*np.log(np.sqrt(np.linalg.det(g_n_lambda) / lambda_ ** d) / delta)) + \
                  np.sqrt(lambda_*d) * param_bound
-        values, P = np.linalg.eig(G_N_lambda)
-        M = np.sqrt(beta_n) * np.linalg.inv(P) @ np.diag(np.sqrt(1 / values))
+        values, pp = np.linalg.eig(g_n_lambda)
+        M = np.sqrt(beta_n) * np.linalg.inv(pp) @ np.diag(np.sqrt(1 / values))
         h = np.array(list(itertools.product([-1, 1], repeat=d)))
         d_theta = [M @ h_k for h_k in h]
-        return theta_N_lambda, d_theta, beta_n, M
+
+        # Clip the parameter and confidence region within the prior parameter box.
+        theta_n_lambda = np.clip(theta_n_lambda, parameter_box[0], parameter_box[1])
+        for k in range(len(d_theta)):
+            d_theta[k] = np.clip(d_theta[k], parameter_box[0] - theta_n_lambda, parameter_box[1] - theta_n_lambda)
+
+        return theta_n_lambda, d_theta, beta_n, M
 
     @staticmethod
-    def is_valid_observation(y, phi, theta):
-        error = y - np.tensordot(theta, phi, axes=[0, 0])
-        print(theta, np.linalg.norm(error))
+    def is_valid_observation(y, phi, theta, beta):
+        y_hat = np.tensordot(theta, phi, axes=[0, 0])
+        error = np.linalg.norm(y - y_hat)
+        eig_values, _ = np.linalg.eig(phi.transpose() @ phi)
+        error_bound = np.sqrt(np.amax(eig_values)) * beta
+        return error < error_bound
 
     @staticmethod
-    def is_consistent_dataset(data):
+    def is_consistent_dataset(data, parameter_box=None):
         train_set = copy.deepcopy(data)
         y, phi = train_set["outputs"].pop(-1), train_set["features"].pop(-1)
+        y, phi = np.array(y)[..., np.newaxis], np.array(phi)[..., np.newaxis]
         if train_set["outputs"] and train_set["features"]:
-            theta, _ = RegressionVehicle.estimate(train_set)
-            RegressionVehicle.is_valid_observation(y, phi, theta)
+            theta, _, beta, _ = RegressionVehicle.parameter_polytope(train_set, delta=0.1,
+                                                                     parameter_box=parameter_box, lambda_=1e-5)
+            return RegressionVehicle.is_valid_observation(y, phi, theta, beta)
+        else:
+            return True
 
     def longitudinal_matrix_polytope(self):
         data = self.get_data()
@@ -56,31 +70,26 @@ class RegressionVehicle(IntervalVehicle):
         if not data:
             return self.parameter_box_to_polytope(parameter_box, structure)
         # Parameters polytope
-        theta_N_lambda, d_theta, _, _ = self.parameter_polytope(data, delta=0.1,
-                                                                param_bound=np.amax(parameter_box[1]))
-        theta_clipped = np.clip(theta_N_lambda, parameter_box[0], parameter_box[1])
-        for k in range(len(d_theta)):
-            d_theta[k] = np.clip(d_theta[k], parameter_box[0] - theta_clipped, parameter_box[1] - theta_clipped)
-
+        theta_n_lambda, d_theta, _, _ = self.parameter_polytope(data, delta=0.1, parameter_box=parameter_box)
         # Structure
         a, phi = structure()
-        a0 = a + np.tensordot(theta_clipped, phi, axes=[0, 0])
+        a0 = a + np.tensordot(theta_n_lambda, phi, axes=[0, 0])
         da = [np.tensordot(d_theta_k, phi, axes=[0, 0]) for d_theta_k in d_theta]
         return a0, da
 
 
 class MultipleModelVehicle(LinearVehicle):
         def __init__(self,
-                 road,
-                 position,
-                 heading=0,
-                 velocity=0,
-                 target_lane_index=None,
-                 target_velocity=None,
-                 route=None,
-                 enable_lane_change=True,
-                 timer=None,
-                 data=None):
+                     road,
+                     position,
+                     heading=0,
+                     velocity=0,
+                     target_lane_index=None,
+                     target_velocity=None,
+                     route=None,
+                     enable_lane_change=True,
+                     timer=None,
+                     data=None):
             super().__init__(road, position, heading, velocity, target_lane_index, target_velocity, route,
                              enable_lane_change, timer, data)
             if not self.data:
@@ -98,7 +107,6 @@ class MultipleModelVehicle(LinearVehicle):
         def collect_data(self):
             for route, data in self.data:
                 self.add_features(data, route[0], output_lane=self.target_lane_index)
-                # print(route[0], "vs", self.target_lane_index, "phi", data["lateral"]["features"][-1], "out", data["lateral"]["outputs"][-1])
 
         def update_possible_routes(self):
             for route in self.get_routes_at_intersection():
@@ -119,10 +127,11 @@ class MultipleModelVehicle(LinearVehicle):
                     route.pop(0)
 
             # Reject hypotheses
-            for route, data in self.data:
+            for route, data in self.data.copy():
                 if data:
-                    print(route)
-                    RegressionVehicle.is_consistent_dataset(data["lateral"])
+                    if not RegressionVehicle.is_consistent_dataset(data["lateral"],
+                                                                   parameter_box=LinearVehicle.STEERING_RANGE):
+                        self.data.remove((route, data))
 
         def assume_model_is_valid(self, index):
             if not self.data:
