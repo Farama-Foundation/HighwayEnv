b56851a4ad8f095f711206a014db2e1290e5f7f4
Edouard Leurent <eleurent@gmail.com>
2020-03-10 16:14:54 +0100

    Add a vehicle dynamics model with tire friction

--- highway_env/vehicle/dynamics.py
+++ highway_env/vehicle/dynamics.py
@@ -1,276 +1,164 @@
-from __future__ import division, print_function
-
-import copy
-
 import numpy as np
-import pandas as pd
-from collections import deque
+import matplotlib.pyplot as plt
 
-from highway_env import utils
-from highway_env.logger import Loggable
+from highway_env.vehicle.kinematics import Vehicle
+
+# vu = 5
+# m = 1
+# Cf = 1
+# Cr = 1
+# theta = [Cf, Cr]
+# a = 2.5
+# b = 2.5
+# width = 2
+# Iz = 1/12 * m * ((a+b)**2 + 3 * width**2)
 
 
-class Vehicle(Loggable):
+class BicycleVehicle(Vehicle):
     """
-        A moving vehicle on a road, and its dynamics.
-
-        The vehicle is represented by a dynamical system: a modified bicycle model.
-        It's state is propagated depending on its steering and acceleration actions.
+        This model is based on the following assumptions:
+        - the vehicle is moving with a constant longitudinal speed
+        - the steering input to front tires and the corresponding slip angles are small
+        See https://pdfs.semanticscholar.org/bb9c/d2892e9327ec1ee647c30c320f2089b290c1.pdf, Chapter 3.
     """
-    COLLISIONS_ENABLED = True
-    """ Enable collision detection between vehicles """
-
-    LENGTH = 5.0
-    """ Vehicle length [m] """
-    WIDTH = 2.0
-    """ Vehicle width [m] """
-    DEFAULT_VELOCITIES = [23, 25]
-    """ Range for random initial velocities [m/s] """
-    MAX_VELOCITY = 40
-    """ Maximum reachable velocity [m/s] """
+    MASS = 1
+    LENGTH_A = Vehicle.LENGTH / 2
+    LENGTH_B = Vehicle.LENGTH / 2
+    INERTIA_Z = 1/12 * MASS * (Vehicle.LENGTH ** 2 + 3 * Vehicle.WIDTH ** 2)
+    FRICTION_FRONT = 1
+    FRICTION_REAR = 1
 
     def __init__(self, road, position, heading=0, velocity=0):
-        self.road = road
-        self.position = np.array(position).astype('float')
-        self.heading = heading
-        self.velocity = velocity
-        self.lane_index = self.road.network.get_closest_lane_index(self.position) if self.road else np.nan
-        self.lane = self.road.network.get_lane(self.lane_index) if self.road else None
-        self.action = {'steering': 0, 'acceleration': 0}
-        self.crashed = False
-        self.log = []
-        self.history = deque(maxlen=30)
-
-    @classmethod
-    def make_on_lane(cls, road, lane_index, longitudinal, velocity=0):
-        """
-            Create a vehicle on a given lane at a longitudinal position.
-
-        :param road: the road where the vehicle is driving
-        :param lane_index: index of the lane where the vehicle is located
-        :param longitudinal: longitudinal position along the lane
-        :param velocity: initial velocity in [m/s]
-        :return: A vehicle with at the specified position
-        """
-        lane = road.network.get_lane(lane_index)
-        if velocity is None:
-            velocity = lane.speed_limit
-        return cls(road, lane.position(longitudinal, 0), lane.heading_at(longitudinal), velocity)
-
-    @classmethod
-    def create_random(cls, road, velocity=None, spacing=1):
-        """
-            Create a random vehicle on the road.
-
-            The lane and /or velocity are chosen randomly, while longitudinal position is chosen behind the last
-            vehicle in the road with density based on the number of lanes.
-
-        :param road: the road where the vehicle is driving
-        :param velocity: initial velocity in [m/s]. If None, will be chosen randomly
-        :param spacing: ratio of spacing to the front vehicle, 1 being the default
-        :return: A vehicle with random position and/or velocity
-        """
-        if velocity is None:
-            velocity = road.np_random.uniform(Vehicle.DEFAULT_VELOCITIES[0], Vehicle.DEFAULT_VELOCITIES[1])
-        default_spacing = 1.5*velocity
-        _from = road.np_random.choice(list(road.network.graph.keys()))
-        _to = road.np_random.choice(list(road.network.graph[_from].keys()))
-        _id = road.np_random.choice(len(road.network.graph[_from][_to]))
-        offset = spacing * default_spacing * np.exp(-5 / 30 * len(road.network.graph[_from][_to]))
-        # x0 = np.max([v.position[0] for v in road.vehicles]) if len(road.vehicles) else 3*offset
-        x0 = len(road.vehicles) * 30 if len(road.vehicles) else -10
-        x0 += offset * road.np_random.uniform(0.9, 1.1)
-        v = cls(road,
-                road.network.get_lane((_from, _to, _id)).position(x0, 0),
-                road.network.get_lane((_from, _to, _id)).heading_at(x0),
-                velocity)
-        return v
-
-    @classmethod
-    def create_from(cls, vehicle):
-        """
-            Create a new vehicle from an existing one.
-            Only the vehicle dynamics are copied, other properties are default.
-
-        :param vehicle: a vehicle
-        :return: a new vehicle at the same dynamical state
-        """
-        v = cls(vehicle.road, vehicle.position, vehicle.heading, vehicle.velocity)
-        return v
-
-    def act(self, action=None):
-        """
-            Store an action to be repeated.
-
-        :param action: the input action
-        """
-        if action:
-            self.action = action
+        super().__init__(road, position, heading, velocity)
+        self.lateral_velocity = 0
+        self.yaw_rate = 0
+        self.A_lat, self.B_lat = self.lateral_lpv_dynamics()
 
     def step(self, dt):
-        """
-            Propagate the vehicle state given its actions.
+        self.clip_actions()
+        x = np.array([[self.lateral_velocity], [self.yaw_rate]])
+        u = np.array([[self.action['steering']]])
+        noise = np.zeros((2, 1))
+        dx = self.A_lat @ x + self.B_lat @ u + noise
 
-            Integrate a modified bicycle model with a 1st-order response on the steering wheel dynamics.
-            If the vehicle is crashed, the actions are overridden with erratic steering and braking until complete stop.
-            The vehicle's current lane is updated.
-
-        :param dt: timestep of integration of the model [s]
-        """
-        if self.crashed:
-            self.action['steering'] = 0
-            self.action['acceleration'] = -1.0*self.velocity
-        self.action['steering'] = float(self.action['steering'])
-        self.action['acceleration'] = float(self.action['acceleration'])
-        if self.velocity > self.MAX_VELOCITY:
-            self.action['acceleration'] = min(self.action['acceleration'], 1.0*(self.MAX_VELOCITY - self.velocity))
-        elif self.velocity < -self.MAX_VELOCITY:
-            self.action['acceleration'] = max(self.action['acceleration'], 1.0*(self.MAX_VELOCITY - self.velocity))
-
-        v = self.velocity * np.array([np.cos(self.heading), np.sin(self.heading)])
-        self.position += v * dt
-        self.heading += self.velocity * np.tan(self.action['steering']) / self.LENGTH * dt
+        c, s = np.cos(self.heading), np.sin(self.heading)
+        R = np.array(((c, -s), (s, c)))
+        velocity = R @ np.array([self.velocity, self.lateral_velocity])
+        self.position += velocity * dt
+        self.heading += dx[1, 0] * dt
         self.velocity += self.action['acceleration'] * dt
+        self.lateral_velocity += dx[0, 0] * dt
+        self.on_state_update()
 
-        if self.road:
-            self.lane_index = self.road.network.get_closest_lane_index(self.position)
-            self.lane = self.road.network.get_lane(self.lane_index)
-            if self.road.record_history:
-                self.history.appendleft(self.create_from(self))
-
-    def lane_distance_to(self, vehicle, lane=None):
+    def lateral_lpv_structure(self):
         """
-            Compute the signed distance to another vehicle along a lane.
-
-        :param vehicle: the other vehicle
-        :param lane: a lane
-        :return: the distance to the other vehicle [m]
+            State: [lateral velocity v, yaw rate r]
+        :return: lateral dynamics dx = (A0 + theta^T phi)x + B u
         """
-        if not vehicle:
-            return np.nan
-        if not lane:
-            lane = self.lane
-        return lane.local_coordinates(vehicle.position)[0] - lane.local_coordinates(self.position)[0]
+        speed_body_x = self.velocity
+        A0 = [
+            [0, -speed_body_x],
+            [0, 0]
+        ]
+        phi = [
+            [
+                [-2 / (self.MASS*speed_body_x), -2*self.LENGTH_A / (self.MASS*speed_body_x)],
+                [-2*self.LENGTH_A / (self.INERTIA_Z*speed_body_x), -2*self.LENGTH_A**2 / (self.INERTIA_Z*speed_body_x)]
+            ], [
+                [-2 / (self.MASS*speed_body_x), 2*self.LENGTH_B / (self.MASS*speed_body_x)],
+                [2*self.LENGTH_B / (self.INERTIA_Z*speed_body_x), -2*self.LENGTH_B**2 / (self.INERTIA_Z*speed_body_x)]
+            ],
+        ]
+        B = [[2*self.FRICTION_FRONT / self.MASS],
+             [self.FRICTION_FRONT * self.LENGTH_A / self.INERTIA_Z]]
+        return A0, phi, B
 
-    def check_collision(self, other):
+    def lateral_lpv_dynamics(self):
         """
-            Check for collision with another vehicle.
-
-        :param other: the other vehicle
+            State: [lateral velocity v, yaw rate r]
+        :return: lateral dynamics A, B
         """
-        if not self.COLLISIONS_ENABLED or not other.COLLISIONS_ENABLED or self.crashed or other is self:
-            return
+        A0, phi, B = self.lateral_lpv_structure()
+        theta = [self.FRICTION_FRONT, self.FRICTION_REAR]
+        A = np.array(A0) + np.tensordot(theta, phi, axes=[0, 0])
+        return A, np.array(B)
 
-        # Fast spherical pre-check
-        if np.linalg.norm(other.position - self.position) > self.LENGTH:
-            return
-
-        # Accurate rectangular check
-        if utils.rotated_rectangles_intersect((self.position, 0.9*self.LENGTH, 0.9*self.WIDTH, self.heading),
-                                              (other.position, 0.9*other.LENGTH, 0.9*other.WIDTH, other.heading)):
-            self.velocity = other.velocity = min([self.velocity, other.velocity], key=abs)
-            self.crashed = other.crashed = True
-
-    @property
-    def direction(self):
-        return np.array([np.cos(self.heading), np.sin(self.heading)])
-
-    @property
-    def destination(self):
-        if getattr(self, "route", None):
-            last_lane = self.road.network.get_lane(self.route[-1])
-            return last_lane.position(last_lane.length, 0)
-        else:
-            return self.position
-
-    @property
-    def destination_direction(self):
-        if (self.destination != self.position).any():
-            return (self.destination - self.position) / np.linalg.norm(self.destination - self.position)
-        else:
-            return np.zeros((2,))
-
-    def front_distance_to(self, other):
-        return self.direction.dot(other.position - self.position)
-
-    def to_dict(self, origin_vehicle=None, observe_intentions=True):
-        d = {
-            'presence': 1,
-            'x': self.position[0],
-            'y': self.position[1],
-            'vx': self.velocity * self.direction[0],
-            'vy': self.velocity * self.direction[1],
-            'cos_h': self.direction[0],
-            'sin_h': self.direction[1],
-            'cos_d': self.destination_direction[0],
-            'sin_d': self.destination_direction[1]
-        }
-        if not observe_intentions:
-            d["cos_d"] = d["sin_d"] = 0
-        if origin_vehicle:
-            origin_dict = origin_vehicle.to_dict()
-            for key in ['x', 'y', 'vx', 'vy']:
-                d[key] -= origin_dict[key]
-        return d
-
-    def dump(self):
+    def full_lateral_lpv_structure(self):
         """
-            Update the internal log of the vehicle, containing:
-                - its kinematics;
-                - some metrics relative to its neighbour vehicles.
+            State: [position y, yaw psi, lateral velocity v, yaw rate r]
+            The system is linearized around psi = 0
+        :return: lateral dynamics A, B
         """
-        data = {
-            'x': self.position[0],
-            'y': self.position[1],
-            'psi': self.heading,
-            'vx': self.velocity * np.cos(self.heading),
-            'vy': self.velocity * np.sin(self.heading),
-            'v': self.velocity,
-            'acceleration': self.action['acceleration'],
-            'steering': self.action['steering']}
+        A_lat, phi_lat, B_lat = self.lateral_lpv_structure()
 
-        if self.road:
-            for lane_index in self.road.network.side_lanes(self.lane_index):
-                lane_coords = self.road.network.get_lane(lane_index).local_coordinates(self.position)
-                data.update({
-                    'dy_lane_{}'.format(lane_index): lane_coords[1],
-                    'psi_lane_{}'.format(lane_index): self.road.network.get_lane(lane_index).heading_at(lane_coords[0])
-                })
-            front_vehicle, rear_vehicle = self.road.neighbour_vehicles(self)
-            if front_vehicle:
-                data.update({
-                    'front_v': front_vehicle.velocity,
-                    'front_distance': self.lane_distance_to(front_vehicle)
-                })
-            if rear_vehicle:
-                data.update({
-                    'rear_v': rear_vehicle.velocity,
-                    'rear_distance': rear_vehicle.lane_distance_to(self)
-                })
+        speed_body_x = self.velocity
+        A_top = np.array([
+            [0, speed_body_x, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        A0 = np.concatenate(A_top, np.concatenate(np.zeros((2, 2)), A_lat, axis=1))
+        phi = [np.concatenate(np.zeros((2, 4)), np.concatenate(np.zeros((2, 2)), phi_i, axis=1)) for phi_i in phi_lat]
+        B = np.concatenate(np.zeros((2, 1)), B_lat)
+        return A0, phi, B
 
-        self.log.append(data)
-
-    def get_log(self):
+    def full_lateral_lpv_dynamics(self):
         """
-            Cast the internal log as a DataFrame.
-
-        :return: the DataFrame of the Vehicle's log.
+            State: [position y, yaw psi, lateral velocity v, yaw rate r]
+            The system is linearized around psi = 0
+        :return: lateral dynamics A, B
         """
-        return pd.DataFrame(self.log)
-
-    def __str__(self):
-        return "{} #{}: {}".format(self.__class__.__name__, id(self) % 1000, self.position)
-
-    def __repr__(self):
-        return self.__str__()
+        A0, phi, B = self.full_lateral_lpv_structure()
+        theta = [self.FRICTION_FRONT, self.FRICTION_REAR]
+        A = np.array(A0) + np.tensordot(theta, phi, axes=[0, 0])
+        return A, np.array(B)
 
 
-class Obstacle(Vehicle):
-    """
-        A motionless obstacle at a given position.
-    """
+def simulate(dt=0.1):
+    time = np.arange(0, 30, dt)
+    vehicle = BicycleVehicle(road=None, position=[0, 5], velocity=5)
+    xx, uu = [], []
+    K = np.array([[1e-1, 2, 0, 1]])
+    for t in time:
+        # Act
+        u = - K @ np.array([[vehicle.position[1]], [vehicle.heading], [vehicle.lateral_velocity], [vehicle.yaw_rate]])
+        omega = 2*np.pi/10
+        u += np.array([[-10*omega*np.sin(omega*t) * dt]])
+        # Record
+        xx.append(np.array([vehicle.position[0], vehicle.position[1], vehicle.heading])[:, np.newaxis])
+        uu.append(u.copy())
+        # Step
+        vehicle.act({"acceleration": 0, "steering": u})
+        vehicle.step(dt)
+    xx, uu = np.array(xx), np.array(uu)
+    plot(time, xx, uu)
 
-    def __init__(self, road, position, heading=0):
-        super(Obstacle, self).__init__(road, position, velocity=0, heading=heading)
-        self.target_velocity = 0
-        self.LENGTH = self.WIDTH
+
+def plot(time, xx, uu):
+    pos_x = xx[:, 0, 0]
+    pos_y = xx[:, 1, 0]
+    psi_x = np.cos(xx[:, 2, 0])
+    psi_y = np.sin(xx[:, 2, 0])
+    dir_x = np.cos(xx[:, 2, 0] + uu[:, 0, 0])
+    dir_y = np.sin(xx[:, 2, 0] + uu[:, 0, 0])
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(pos_x, pos_y)
+    dir_scale = 1/5
+    ax.quiver(pos_x[::20]-0.5/dir_scale*psi_x[::20],
+              pos_y[::20]-0.5/dir_scale*psi_y[::20],
+              psi_x[::20], psi_y[::20],
+              angles='xy', scale_units='xy', scale=dir_scale, width=0.005, headwidth=1)
+    ax.quiver(pos_x[::20]+0.5/dir_scale*psi_x[::20], pos_y[::20]+0.5/dir_scale*psi_y[::20], dir_x[::20], dir_y[::20],
+              angles='xy', scale_units='xy', scale=0.25, width=0.005, color='r')
+    ax.axis("equal")
+    ax.grid()
+    # ax1.plot(pos_x, xx[:, 3, 0])
+    plt.show()
+    plt.close()
+
+
+def main():
+    simulate()
+
+
+if __name__ == '__main__':
+    main()
