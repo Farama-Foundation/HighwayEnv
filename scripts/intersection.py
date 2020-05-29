d82d65124dfddbf157d3f1324e3471c01ee26895
Isaac Kargar <kargarisaac@yahoo.com>
2020-05-29 11:23:24 +0300

    add ppo test

--- scripts/intersection.py
+++ scripts/intersection.py
@@ -7,27 +7,23 @@ from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.deepq.policies import MlpPolicy as DQNMlp
 from stable_baselines.deepq.policies import CnnPolicy as DQNCnn
 from stable_baselines import PPO2, DQN
-
+from stable_baselines.common.policies import CnnPolicy
 
 if __name__ == '__main__':
-    # env = SubprocVecEnv([lambda: gym.make("intersection-v1") for i in range(1)])
-    # policy_kwargs = {}
-    # model = DQN(DQNCnn, env,
-    #             verbose=1,
-    #             policy_kwargs=policy_kwargs,
-    #             batch_size=32,
-    #             exploration_fraction=0.3,
-    #             learning_rate=1e-4,
-    #             tensorboard_log="./logs/"
-    #             )
-    # model.learn(total_timesteps=int(1e6))
-    # model.save("deepq_intersection_v1")
+    env = SubprocVecEnv([lambda: gym.make("intersection-v2") for i in range(2)])
+    model = PPO2(CnnPolicy,
+                 env,
+                 verbose=1,
+                 tensorboard_log="./logs/"
+                )
+    model.learn(total_timesteps=int(1e6))
+    model.save("ppo2_intersection_v2")
 
-    env = gym.make("intersection-v2")
-    done = False
-    env.reset()
-    while not done:
-        # action = np.array([0, 0]) #acc, steer
-        action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
-        env.render()
+    # env = gym.make("intersection-v2")
+    # done = False
+    # env.reset()
+    # while not done:
+    #     # action = np.array([0, 0]) #acc, steer
+    #     action = env.action_space.sample()
+    #     obs, reward, done, _ = env.step(action)
+    #     env.render()
