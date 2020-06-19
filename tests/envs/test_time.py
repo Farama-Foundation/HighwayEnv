93d06332817f1e1c31291691420e28ca8da22efc
Edouard Leurent <eleurent@gmail.com>
2020-06-19 19:47:43 +0200

    Add a test for running time
    This will also be used as a profiler for future optimisations

---
+++ tests/envs/test_time.py
@@ -0,0 +1,38 @@
+import pytest
+import timeit
+import gym
+
+import highway_env
+
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+
+def time_env(env_name, steps=20):
+    env = gym.make(env_name)
+    env.reset()
+    for _ in range(steps):
+        _, _, done, _ = env.step(env.action_space.sample())
+        env.reset() if done else _
+    env.close()
+
+
+def test_running_time(repeat=1):
+    for env_name, steps in [
+        ("highway-v0", 10),
+        ("parking-v0", 20)
+    ]:
+        env_time = wrapper(time_env, env_name, steps)
+        time_spent = timeit.timeit(env_time, number=repeat) / repeat
+        env = gym.make(env_name)
+        time_simulated = steps / env.unwrapped.config["policy_frequency"]
+        real_time_ratio = time_simulated / time_spent
+        print("Real time ratio for {}: {}".format(env_name, real_time_ratio))
+        assert real_time_ratio > 0.5  # let's not be too ambitious for now
+
+
+if __name__ == "__main__":
+    test_running_time()
