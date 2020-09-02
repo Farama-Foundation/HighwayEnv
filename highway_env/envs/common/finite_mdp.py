b578be9fda2442ca6960d979e3673e2cd40be9c7
Edouard Leurent <eleurent@gmail.com>
2020-09-02 15:22:07 +0200

    Add the MultiAgentObservation

--- highway_env/envs/common/finite_mdp.py
+++ highway_env/envs/common/finite_mdp.py
@@ -1,10 +1,11 @@
 import importlib
 from functools import partial
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 import numpy as np
 
 from highway_env import utils
+from highway_env.vehicle.kinematics import Vehicle
 
 if TYPE_CHECKING:
     from highway_env.envs import AbstractEnv
@@ -77,32 +78,40 @@ def finite_mdp(env: 'AbstractEnv',
         raise ModuleNotFoundError("The finite_mdp module is required for conversion. {}".format(e))
 
 
-def compute_ttc_grid(env: 'AbstractEnv', time_quantization: float, horizon: float, considered_lanes: str = "all") \
-        -> np.ndarray:
+def compute_ttc_grid(env: 'AbstractEnv',
+                     time_quantization: float,
+                     horizon: float,
+                     vehicle: Optional[Vehicle] = None) -> np.ndarray:
     """
     Compute the grid of predicted time-to-collision to each vehicle within the lane
 
     For each ego-speed and lane.
+    :param env: environment
+    :param time_quantization: time step of a grid cell
+    :param horizon: time horizon of the grid
+    :param vehicle: the observer vehicle
+    :return: the time-co-collision grid, with axes SPEED x LANES x TIME
     """
+    vehicle = vehicle or env.vehicle
     road_lanes = env.road.network.all_side_lanes(env.vehicle.lane_index)
-    grid = np.zeros((env.vehicle.SPEED_COUNT, len(road_lanes), int(horizon / time_quantization)))
+    grid = np.zeros((vehicle.SPEED_COUNT, len(road_lanes), int(horizon / time_quantization)))
     for speed_index in range(grid.shape[0]):
-        ego_speed = env.vehicle.index_to_speed(speed_index)
+        ego_speed = vehicle.index_to_speed(speed_index)
         for other in env.road.vehicles:
-            if (other is env.vehicle) or (ego_speed == other.speed):
+            if (other is vehicle) or (ego_speed == other.speed):
                 continue
-            margin = other.LENGTH / 2 + env.vehicle.LENGTH / 2
+            margin = other.LENGTH / 2 + vehicle.LENGTH / 2
             collision_points = [(0, 1), (-margin, 0.5), (margin, 0.5)]
             for m, cost in collision_points:
-                distance = env.vehicle.lane_distance_to(other) + m
-                other_projected_speed = other.speed * np.dot(other.direction, env.vehicle.direction)
+                distance = vehicle.lane_distance_to(other) + m
+                other_projected_speed = other.speed * np.dot(other.direction, vehicle.direction)
                 time_to_collision = distance / utils.not_zero(ego_speed - other_projected_speed)
                 if time_to_collision < 0:
                     continue
-                if env.road.network.is_connected_road(env.vehicle.lane_index, other.lane_index,
-                                                      route=env.vehicle.route, depth=3):
+                if env.road.network.is_connected_road(vehicle.lane_index, other.lane_index,
+                                                      route=vehicle.route, depth=3):
                     # Same road, or connected road with same number of lanes
-                    if len(env.road.network.all_side_lanes(other.lane_index)) == len(env.road.network.all_side_lanes(env.vehicle.lane_index)):
+                    if len(env.road.network.all_side_lanes(other.lane_index)) == len(env.road.network.all_side_lanes(vehicle.lane_index)):
                         lane = [other.lane_index[2]]
                     # Different road of different number of lanes: uncertainty on future lane, use all
                     else:
