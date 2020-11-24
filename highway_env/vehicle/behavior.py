391c15793d4062157a04813dc0234e4008b6fbf4
Edouard Leurent <eleurent@gmail.com>
2020-11-24 14:41:08 +0100

    IDM: use projected velocity difference rather than speed difference
    Fix #117

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -144,18 +144,20 @@ class IDMVehicle(ControlledVehicle):
                 np.power(self.desired_gap(ego_vehicle, front_vehicle) / utils.not_zero(d), 2)
         return acceleration
 
-    def desired_gap(self, ego_vehicle: Vehicle, front_vehicle: Vehicle = None) -> float:
+    def desired_gap(self, ego_vehicle: Vehicle, front_vehicle: Vehicle = None, projected: bool = True) -> float:
         """
         Compute the desired distance between a vehicle and its leading vehicle.
 
         :param ego_vehicle: the vehicle being controlled
         :param front_vehicle: its leading vehicle
+        :param projected: project 2D velocities in 1D space
         :return: the desired distance between the two [m]
         """
         d0 = self.DISTANCE_WANTED
         tau = self.TIME_WANTED
         ab = -self.COMFORT_ACC_MAX * self.COMFORT_ACC_MIN
-        dv = ego_vehicle.speed - front_vehicle.speed
+        dv = np.dot(ego_vehicle.velocity - front_vehicle.velocity, ego_vehicle.direction) if projected \
+            else ego_vehicle.speed - front_vehicle.speed
         d_star = d0 + ego_vehicle.speed * tau + ego_vehicle.speed * dv / (2 * np.sqrt(ab))
         return d_star
 
