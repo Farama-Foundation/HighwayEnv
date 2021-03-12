4a214814925270650680a02f6afb332990e9f405
Edouard Leurent <eleurent@gmail.com>
2021-03-12 17:08:19 +0100

    Add dummy setup.py back to support pip editable mode

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Following PEP 517/518, this file should not not needed and replaced instead by the setup.cfg file and pyproject.toml.
+# Unfortunately it is still required py the pip editable mode `pip install -e`
+# See https://stackoverflow.com/a/60885212
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
