750b961289f9f048eb7d925ade7df385a451d0fb
Edouard Leurent <eleurent@gmail.com>
2021-03-21 18:00:13 +0100

    Fix channel convention in grayscale obs test
    See #147

--- tests/graphics/test_render.py
+++ tests/graphics/test_render.py
@@ -18,18 +18,18 @@ def test_render(env_spec):
 
 
 @pytest.mark.parametrize("env_spec", envs)
-def test_obs_grayscale(env_spec):
+def test_obs_grayscale(env_spec, stack_size=4):
     env = gym.make(env_spec)
     env.configure({
         "offscreen_rendering": True,
         "observation": {
             "type": "GrayscaleObservation",
+            "observation_shape": (env.config["screen_width"], env.config["screen_height"]),
+            "stack_size": stack_size,
             "weights": [0.2989, 0.5870, 0.1140],
-            "stack_size": 4,
-            "observation_shape": (env.config["screen_width"], env.config["screen_height"])
         }
     })
     obs = env.reset()
     env.close()
     assert isinstance(obs, np.ndarray)
-    assert obs.shape == (env.config["screen_width"], env.config["screen_height"], 4)
+    assert obs.shape == (stack_size, env.config["screen_width"], env.config["screen_height"])
