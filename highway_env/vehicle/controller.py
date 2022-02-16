c80e2a21b1fbe868fe6d21ad5ed668bed076376f
Edouard Leurent <eleurent@gmail.com>
2022-02-16 23:50:45 +0000

    Fix conversion from heading rate to steering wheel angle in controller
    Fix https://github.com/eleurent/highway-env/issues/262

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -138,8 +138,8 @@ class ControlledVehicle(Vehicle):
         # Heading control
         heading_rate_command = self.KP_HEADING * utils.wrap_to_pi(heading_ref - self.heading)
         # Heading rate to steering angle
-        steering_angle = np.arcsin(np.clip(self.LENGTH / 2 / utils.not_zero(self.speed) * heading_rate_command,
-                                           -1, 1))
+        slip_angle = np.arcsin(np.clip(self.LENGTH / 2 / utils.not_zero(self.speed) * heading_rate_command, -1, 1))
+        steering_angle = np.arctan(2 * np.tan(slip_angle))
         steering_angle = np.clip(steering_angle, -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
         return float(steering_angle)
 
