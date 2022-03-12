236c2c37f75dc052d94336ab97fc5a64f4eea0a8
Edouard Leurent <eleurent@gmail.com>
2022-03-12 17:52:43 +0000

    Set intersection env's offroad terminal config as false by default

--- highway_env/envs/intersection_env.py
+++ highway_env/envs/intersection_env.py
@@ -59,7 +59,7 @@ class IntersectionEnv(AbstractEnv):
             "arrived_reward": 1,
             "reward_speed_range": [7.0, 9.0],
             "normalize_reward": False,
-            "offroad_terminal": True
+            "offroad_terminal": False
         })
         return config
 
