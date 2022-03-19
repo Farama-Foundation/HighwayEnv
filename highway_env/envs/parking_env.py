f8a4c633012ee004786346908e3f9b841edd64aa
Edouard Leurent <eleurent@gmail.com>
2022-03-19 15:26:32 +0000

    Drop gym_robotics requirements
    Not allowed by pypi

--- highway_env/envs/parking_env.py
+++ highway_env/envs/parking_env.py
@@ -1,5 +1,6 @@
+from abc import abstractmethod
+from gym import Env
 from gym.envs.registration import register
-from gym_robotics import GoalEnv
 import numpy as np
 
 from highway_env.envs.common.abstract import AbstractEnv
@@ -7,6 +8,43 @@ from highway_env.envs.common.observation import MultiAgentObservation, observati
 from highway_env.road.lane import StraightLane, LineType
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.objects import Landmark
+
+
+class GoalEnv(Env):
+    """
+    Interface for A goal-based environment.
+
+    This interface is needed by agents such as Stable Baseline3's Hindsight Experience Replay (HER) agent.
+    It was originally part of https://github.com/openai/gym, but was later moved
+    to https://github.com/Farama-Foundation/gym-robotics. We cannot add gym-robotics to this project's dependencies,
+    since it does not have an official PyPi package, PyPi does not allow direct dependencies to git repositories.
+    So instead, we just reproduce the interface here.
+
+    A goal-based environment. It functions just as any regular OpenAI Gym environment but it
+    imposes a required structure on the observation_space. More concretely, the observation
+    space is required to contain at least three elements, namely `observation`, `desired_goal`, and
+    `achieved_goal`. Here, `desired_goal` specifies the goal that the agent should attempt to achieve.
+    `achieved_goal` is the goal that it currently achieved instead. `observation` contains the
+    actual observations of the environment as per usual.
+    """
+
+    @abstractmethod
+    def compute_reward(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info: dict) -> float:
+        """Compute the step reward. This externalizes the reward function and makes
+        it dependent on a desired goal and the one that was achieved. If you wish to include
+        additional rewards that are independent of the goal, you can include the necessary values
+        to derive it in 'info' and compute it accordingly.
+        Args:
+            achieved_goal (object): the goal that was achieved during execution
+            desired_goal (object): the desired goal that we asked the agent to attempt to achieve
+            info (dict): an info dictionary with additional information
+        Returns:
+            float: The reward that corresponds to the provided achieved goal w.r.t. to the desired
+            goal. Note that the following should always hold true:
+                ob, reward, done, info = env.step()
+                assert reward == env.compute_reward(ob['achieved_goal'], ob['desired_goal'], info)
+        """
+        raise NotImplementedError
 
 
 class ParkingEnv(AbstractEnv, GoalEnv):
