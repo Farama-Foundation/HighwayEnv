2584e59994beddf4eaee45ea35258d08b500c75e
lorandcheng <lfcheng@usc.edu>
2022-08-01 10:23:38 -0500

    updating inheritance

--- highway_env/envs/mo_highway_env.py
+++ highway_env/envs/mo_highway_env.py
@@ -1,16 +1,12 @@
 import numpy as np
 from gym.envs.registration import register
 
-from highway_env import utils
+from highway_env import HighwayEnv, utils
 from highway_env.envs.common.abstract import MOAbstractEnv
 from highway_env.envs.common.action import Action
-from highway_env.road.road import Road, RoadNetwork
-from highway_env.utils import near_split
 from highway_env.vehicle.controller import ControlledVehicle
-from highway_env.vehicle.kinematics import Vehicle
 
-
-class MOHighwayEnv(MOAbstractEnv):
+class MOHighwayEnv(MOAbstractEnv, HighwayEnv):
     """
     A multi-objective version of HighwayEnv
     """
@@ -19,65 +15,6 @@ class MOHighwayEnv(MOAbstractEnv):
         self._add_reward("collision", self._collision_reward)
         self._add_reward("right_lane", self._right_lane_reward)
         self._add_reward("high_speed", self._high_speed_reward)
-        
-    @classmethod
-    def default_config(cls) -> dict:
-        config = super().default_config()
-        config.update({
-            "observation": {
-                "type": "Kinematics"
-            },
-            "action": {
-                "type": "DiscreteMetaAction",
-            },
-            "lanes_count": 4,
-            "vehicles_count": 50,
-            "controlled_vehicles": 1,
-            "initial_lane_id": None,
-            "duration": 40,  # [s]
-            "ego_spacing": 2,
-            "vehicles_density": 1,
-            "collision_reward": -1,    # The reward received when colliding with a vehicle.
-            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
-                                       # zero for other lanes.
-            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
-                                       # lower speeds according to config["reward_speed_range"].
-            "lane_change_reward": 0,   # The reward received at each lane change action.
-            "reward_speed_range": [20, 30],
-            "offroad_terminal": False
-        })
-        return config
-
-    def _reset(self) -> None:
-        self._create_road()
-        self._create_vehicles()
-
-    def _create_road(self) -> None:
-        """Create a road composed of straight adjacent lanes."""
-        self.road = Road(network=RoadNetwork.straight_road_network(self.config["lanes_count"], speed_limit=30),
-                         np_random=self.np_random, record_history=self.config["show_trajectories"])
-
-    def _create_vehicles(self) -> None:
-        """Create some new random vehicles of a given type, and add them on the road."""
-        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
-        other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
-
-        self.controlled_vehicles = []
-        for others in other_per_controlled:
-            vehicle = Vehicle.create_random(
-                self.road,
-                speed=25,
-                lane_id=self.config["initial_lane_id"],
-                spacing=self.config["ego_spacing"]
-            )
-            vehicle = self.action_type.vehicle_class(self.road, vehicle.position, vehicle.heading, vehicle.speed)
-            self.controlled_vehicles.append(vehicle)
-            self.road.vehicles.append(vehicle)
-
-            for _ in range(others):
-                vehicle = other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
-                vehicle.randomize_behavior()
-                self.road.vehicles.append(vehicle)
 
     def _collision_reward(self, action: Action) -> float:
         return self.vehicle.crashed
@@ -110,17 +47,6 @@ class MOHighwayEnv(MOAbstractEnv):
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
         return reward
-
-    def _is_terminal(self) -> bool:
-        """The episode is over if the ego vehicle crashed or the time is out."""
-        return self.vehicle.crashed or \
-            self.time >= self.config["duration"] or \
-            (self.config["offroad_terminal"] and not self.vehicle.on_road)
-
-    def _cost(self, action: int) -> float:
-        """The cost signal is the occurrence of collision."""
-        return float(self.vehicle.crashed)
-
 
 class MOHighwayEnvFast(MOHighwayEnv):
     """
