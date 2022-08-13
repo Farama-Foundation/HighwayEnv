42766620afecc4744744dfbd55a2776870eba46b
pranchi <francis.sunny.25@gmail.com>
2022-08-13 14:08:13 +0100

    adding new environment with obstacles

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -8,6 +8,7 @@ from highway_env.road.road import Road, RoadNetwork
 from highway_env.utils import near_split
 from highway_env.vehicle.controller import ControlledVehicle
 from highway_env.vehicle.kinematics import Vehicle
+from highway_env.vehicle.objects import Obstacle
 
 Observation = np.ndarray
 
@@ -197,6 +198,128 @@ class MOHighwayEnv(HighwayEnv):
         """
         return self._rewards(action)
 
+
+class HighwayEnvObstacle(HighwayEnv):
+
+    @classmethod
+    def default_config(cls) -> dict:
+        conf = super().default_config()
+        conf.update({
+            "observation": {
+                "type": "Kinematics"
+            },
+            "action": {
+                "type": "DiscreteMetaAction",
+            },
+            'screen_width': 1200,
+            'screen_height': 400,
+            "lanes_count": 3,
+            "obstacle_count": 200,
+            "vehicles_count": 10,
+            "controlled_vehicles": 2,
+            "initial_lane_id": None,
+            "duration": 30,  # [s]
+            "ego_spacing": 1,
+            "vehicles_density": 1,
+            "collision_reward": -1,  # The reward received when colliding with a vehicle.
+            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
+            # zero for other lanes.
+            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+            # lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,  # The reward received at each lane change action.
+            "reward_speed_range": [20, 30],
+            "offroad_terminal": False,
+            "any_collision_terminal": False
+        })
+        return conf
+
+    def _create_vehicles(self) -> None:
+        """Create some new random vehicles of a given type, and add them on the road."""
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+        other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
+
+        self.controlled_vehicles = []
+        vehicle_dist = 0.0
+        for others in other_per_controlled:
+            vehicle = Vehicle.create_random(
+                self.road,
+                speed=25,
+                lane_id=self.config["initial_lane_id"],
+                spacing=self.config["ego_spacing"]
+            )
+            vehicle = self.action_type.vehicle_class(self.road, vehicle.position, vehicle.heading, vehicle.speed)
+            if self.config['controlled_vehicles']:
+                lanes = [4 * lane for lane in range(self.config["lanes_count"])]
+                vehicle_lane = np.random.choice(lanes)
+                vehicle.position = np.array([vehicle_dist, vehicle_lane])
+                vehicle_dist += 25
+                self.controlled_vehicles.append(vehicle)
+                self.road.vehicles.append(vehicle)
+            else:
+                self.controlled_vehicles.append(vehicle)
+            for _ in range(others):
+                vehicle = other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
+                vehicle.randomize_behavior()
+                self.road.vehicles.append(vehicle)
+
+    def _create_road(self) -> None:
+        """Create a road composed of straight adjacent lanes."""
+        self.road = Road(network=RoadNetwork.straight_road_network(self.config["lanes_count"], speed_limit=30),
+                         np_random=self.np_random, record_history=self.config["show_trajectories"])
+        for i in range(1, self.config['obstacle_count']):
+            lanes = [4 * lane for lane in range(self.config["lanes_count"])]
+            obstacle_lane = np.random.choice(lanes)
+            obstacle_dist = np.random.randint(300, 10000)
+            self.road.objects.append(Obstacle(self.road, [obstacle_dist, obstacle_lane]))
+
+    def _info(self, obs: np.ndarray, action: int) -> dict:
+        info = super()._info(obs, action)
+        info["other_vehicle_collision"] = \
+            sum(vehicle.crashed for vehicle in self.road.vehicles if vehicle not in self.controlled_vehicles)
+        info["agents_rewards"] = tuple(self._agent_reward(action, vehicle) for vehicle in self.controlled_vehicles)
+        info["agents_collided"] = tuple(self._agent_is_terminal(vehicle) for vehicle in self.controlled_vehicles)
+        info['distance_travelled'] = tuple(vehicle.position[0] for vehicle in self.controlled_vehicles)
+        return info
+
+    def _is_terminal(self) -> bool:
+        if self.config['any_collision_terminal']:
+            return any(vehicle.crashed for vehicle in self.controlled_vehicles) or \
+                   self.time >= self.config["duration"] or \
+                   (self.config["offroad_terminal"] and not self.vehicle.on_road)
+        else:
+            return all(vehicle.crashed for vehicle in self.controlled_vehicles) or \
+                   self.time >= self.config["duration"] or \
+                   (self.config["offroad_terminal"] and not self.vehicle.on_road)
+
+    def _agent_is_terminal(self, vehicle: Vehicle) -> bool:
+        """The episode is over when a collision occurs or when the access ramp has been passed."""
+        return vehicle.crashed \
+               or self.time >= self.config["duration"]
+
+    def _reward(self, action: int) -> float:
+        # Cooperative multi-agent reward
+        return sum(self._agent_reward(action, vehicle) for vehicle in self.controlled_vehicles) \
+               / len(self.controlled_vehicles)
+
+    def _agent_reward(self, action: int, vehicle: Vehicle) -> float:
+        neighbours = self.road.network.all_side_lanes(vehicle.lane_index)
+        lane = vehicle.target_lane_index[2] if isinstance(vehicle, ControlledVehicle) \
+            else vehicle.lane_index[2]
+        # Use forward speed rather than speed, see https://github.com/eleurent/highway-env/issues/268
+        forward_speed = vehicle.speed * np.cos(vehicle.heading)
+        scaled_speed = utils.lmap(forward_speed, self.config["reward_speed_range"], [0, 1])
+        reward = \
+            + self.config["collision_reward"] * vehicle.crashed \
+            + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
+            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
+        reward = utils.lmap(reward,
+                            [self.config["collision_reward"],
+                             self.config["high_speed_reward"] + self.config["right_lane_reward"]],
+                            [0, 1])
+        reward = 0 if not vehicle.on_road else reward
+        return reward
+
+
 register(
     id='highway-v0',
     entry_point='highway_env.envs:HighwayEnv',
@@ -210,4 +333,9 @@ register(
 register(
     id='mo-highway-v0',
     entry_point='highway_env.envs:MOHighwayEnv',
+)
+
+register(
+    id='highway-obstacle-v0',
+    entry_point='highway_env.envs:HighwayEnvObstacle',
 )
