d4aa4f6d0ffa27324a0309cfbd66ccc1d3f0d29a
Edouard Leurent <eleurent@gmail.com>
2023-04-14 14:12:37 +0000

    Add custom 404 page in docs

---
+++ docs/scripts/move_404.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Provide a path")
+    filePath = sys.argv[1]
+
+    with open(filePath, "r+") as fp:
+        content = fp.read()
+        content = content.replace('href="../', 'href="/').replace('src="../', 'src="/')
+        fp.seek(0)
+        fp.truncate()
+
+        fp.write(content)
