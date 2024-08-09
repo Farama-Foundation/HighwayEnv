7f58de5a9b678b3ba6f07f3a1147d82f7eb4b053
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2024-08-09 23:36:48 +0100

    Pre-commit

--- highway_env/envs/exit_env.py
+++ highway_env/envs/exit_env.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import numpy as np
 
 from highway_env import utils
-from highway_env.envs.highway_env import HighwayEnv
 from highway_env.envs.common.action import Action
+from highway_env.envs.highway_env import HighwayEnv
 from highway_env.road.lane import CircularLane
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.controller import ControlledVehicle
