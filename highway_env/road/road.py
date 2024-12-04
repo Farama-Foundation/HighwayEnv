e22f081ca7b89b4563a934c84fcf3f10ad851d6b
Gustav C. R. Groenborg <gustavrisagerus@gmail.com>
2024-12-04 11:08:15 +0100

    Quickfix of IndexError

--- highway_env/road/road.py
+++ highway_env/road/road.py
@@ -57,25 +57,10 @@ class RoadNetwork:
         _from, _to, _id = index
         if _id is None:
             pass
-        if _id is None and len(self.graph[_from][_to]) == 1:
+        if _id is None or len(self.graph[_from][_to]) == 1:
             _id = 0
 
-        try:
-            return self.graph[_from][_to][_id]
-        except IndexError:
-            """
-            print("-------------------------------")
-            print(f"IndexError: _from={_from}, _to={_to}, _id={_id}")
-            print("from:")
-            print(self.graph[_from])
-            print("to:")
-            print(self.graph[_from][_to])
-            print("expected: ")
-            print(self.graph[_from][_to][0])
-            print("-------------------------------")
-            """
-            return self.graph[_from][_to][0]
-
+        return self.graph[_from][_to][_id]
 
     def get_closest_lane_index(
         self, position: np.ndarray, heading: float | None = None
